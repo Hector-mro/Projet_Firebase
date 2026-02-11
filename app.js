cd2cba33711a062bc3c23b1afeddf48d5e184132
Nicolas Blaye <gabnicolasblaye@gmail.com>
2026-02-11 17:02:19 +0100

    discord

---
+++ app.js
@@ -0,0 +1,18 @@
+import {} from "firebase/app";
+import {} from "firebase/firestore";
+import {} from "firebase/auth";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAH4aeG3TEpjwzCWvTETCwiBwW5Y3nGFpI",
+  authDomain: "inf6-4db19.firebaseapp.com",
+  projectId: "inf6-4db19",
+  storageBucket: "inf6-4db19.firebasestorage.app",
+  messagingSenderId: "782015273736",
+  appId: "1:782015273736:web:1ffc01e8e8d93578b26abd",
+  measurementId: "G-BFXHFYMN4G",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
