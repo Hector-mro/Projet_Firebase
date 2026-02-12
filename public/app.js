604ba871b88d0f72e806038bf22e421fd9d81ea0
Nicolas Blaye <gabnicolasblaye@gmail.com>
2026-02-12 10:21:05 +0100

    fonctionne

--- public/app.js
+++ public/app.js
@@ -1,12 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  signInWithEmailAndPassword, 
-  signOut, 
-  onAuthStateChanged 
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore,collection, addDoc, serverTimestamp, query, orderBy, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 // Your web app's Firebase configuration 
 // // For Firebase JS SDK v7.20.0 and later, measurementId is optional 
@@ -85,4 +79,54 @@ onAuthStateChanged(auth, (user) => {
     publicationSection.style.display = "none";
     userEmail.textContent = "";
   }
+});
+
+const postForm = document.getElementById("post-form");
+
+postForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const content = document.getElementById("post-content").value;
+  const user = auth.currentUser;
+
+  if (!user) {
+    alert("Vous devez être connecté pour publier.");
+    return;
+  }
+
+  try {
+    await addDoc(collection(db, "messages"), {
+      text: content,
+      userEmail: user.email,
+      createdAt: serverTimestamp()
+    });
+
+    postForm.reset();
+  } catch (error) {
+    alert(error.message);
+  }
+});
+
+const messagesList = document.getElementById("messages-list");
+
+const q = query(
+  collection(db, "messages"),
+  orderBy("createdAt", "desc")
+);
+
+onSnapshot(q, (snapshot) => {
+  messagesList.innerHTML = "";
+
+  snapshot.forEach((doc) => {
+    const message = doc.data();
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+      <strong>${message.userEmail}</strong>
+      <p>${message.text}</p>
+      <hr>
+    `;
+
+    messagesList.appendChild(div);
+  });
 });
